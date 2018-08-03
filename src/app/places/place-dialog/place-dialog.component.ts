3f007e1732e01f53e20aa54ae11020cc01448260
Constantin Bejenaru <cbejenaru@starlab.md>
2018-08-03 16:54:06 +0300

    Create dialog instance. Add button to open details

---
+++ src/app/places/place-dialog/place-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-dialog',
+  templateUrl: './place-dialog.component.html',
+  styleUrls: ['./place-dialog.component.css']
+})
+export class PlaceDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
